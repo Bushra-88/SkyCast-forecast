4d0cfa25de1c214103b344341e78c931788659b6
Bushra Alhusaini <alhusaini.b2023@gmail.com>
2024-08-25 20:09:35 +0200

    Add the degree symbol

--- src/script.js
+++ src/script.js
@@ -100,11 +100,11 @@ function displayForecast(response) {
              </div>
              <div class="weather-forecast-temperatures">
                <div class="weather-forecast-temperature">
-                 <strong>${Math.round(day.temperature.maximum)}</strong>
+                 <strong>${Math.round(day.temperature.maximum)}°</strong>
                </div>
                <div class="weather-forecast-temperature">${Math.round(
                  day.temperature.minimum
-               )}</div>
+               )}°</div>
              </div>
            </div>
      `;
