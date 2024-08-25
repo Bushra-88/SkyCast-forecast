39df8b90150ab099bf757fb242019baa0ff9252f
Bushra Alhusaini <alhusaini.b2023@gmail.com>
2024-08-25 16:40:18 +0200

    Adding the first version of the weather app

---
+++ src/script.js
@@ -0,0 +1,70 @@
+function refreshWeather(response) {
+  let cityElement = document.querySelector("#city");
+  let city = response.data.city;
+
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
+  let descriptionElement = document.querySelector("#description");
+  let description = response.data.condition.description;
+
+  let humidityElement = document.querySelector("#humidity");
+  let humidity = `${response.data.temperature.humidity}%`;
+
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let windSpeed = `${response.data.wind.speed} km/h`;
+
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+
+  let iconElement = document.querySelector("#icon");
+
+  cityElement.innerHTML = city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = description;
+  humidityElement.innerHTML = humidity;
+  windSpeedElement.innerHTML = windSpeed;
+  temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}"class="weather-app-icon" />`;
+}
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    " Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  return `${day} ${hours}:${minutes}`;
+}
+
+function searchCity(city) {
+  let apiKey = "6b8a7398o06094tf908737c5b44dce83";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function handelSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+
+  searchCity(searchInput.value);
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handelSearchSubmit);
+searchCity("Ghent");
