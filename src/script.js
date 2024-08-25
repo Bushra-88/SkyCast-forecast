0327c1b6c632307a0398a15564f6a5e692a8fc08
Bushra Alhusaini <alhusaini.b2023@gmail.com>
2024-08-25 20:07:07 +0200

    Add the day,icon and temperature from th api

--- src/script.js
+++ src/script.js
@@ -60,7 +60,7 @@ function searchCity(city) {
   axios.get(apiUrl).then(refreshWeather);
 }
 
-function handelSearchSubmit(event) {
+function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
 
@@ -71,36 +71,49 @@ function handelSearchSubmit(event) {
 function getForecast(city) {
   let apiKey = "6b8a7398o06094tf908737c5b44dce83";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
-  // console.log(apiUrl);
+
   axios(apiUrl).then(displayForecast);
+}
+
+//Get format day
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[(date.getDay() + 1) % 7]; // +1 to make sure the forecast start from the next day 'not today' and the %7 to  ensure that the day of the week wraps around to Sunday (0)
 }
 
 //Build the forecast
 function displayForecast(response) {
-  console.log(response.data);
-  let days = ["Mon", "Tus", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
   //Use the loop to create html for each day
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
-    <div class="weather-forecast-day">
-           <div class="weather-forecast-date">${day}</div>
-           <div class="weather-forecast-icon">🌤️</div>
-           <div class="weather-forecast-temperatures">
-             <div class="weather-forecast-temperature">
-               <strong>15º</strong>
+  response.data.daily.forEach(function (day, index) {
+    if (index < 6) {
+      forecastHtml =
+        forecastHtml +
+        `
+      <div class="weather-forecast-day">
+             <div class="weather-forecast-date">${formatDay(day.time)}</div>
+             <div>
+             <img src= "${
+               day.condition.icon_url
+             }"  class="weather-forecast-icon"/>
              </div>
-             <div class="weather-forecast-temperature">9º</div>
+             <div class="weather-forecast-temperatures">
+               <div class="weather-forecast-temperature">
+                 <strong>${Math.round(day.temperature.maximum)}</strong>
+               </div>
+               <div class="weather-forecast-temperature">${Math.round(
+                 day.temperature.minimum
+               )}</div>
+             </div>
            </div>
-         </div>
-   `;
+     `;
+    }
   });
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handelSearchSubmit);
+searchFormElement.addEventListener("submit", handleSearchSubmit);
 searchCity("Ghent");
