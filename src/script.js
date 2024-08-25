e0c217e9232121d34866839d29b2c713854e9073
Bushra Alhusaini <alhusaini.b2023@gmail.com>
2024-08-25 20:07:55 +0200

    Update how many day to display for forecast

--- src/script.js
+++ src/script.js
@@ -87,7 +87,7 @@ function displayForecast(response) {
   let forecastHtml = "";
   //Use the loop to create html for each day
   response.data.daily.forEach(function (day, index) {
-    if (index < 6) {
+    if (index < 5) {
       forecastHtml =
         forecastHtml +
         `
